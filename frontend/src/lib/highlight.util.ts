89f27667281711d42a39d17e64c50b1b66be0d49
Ayaka Miyazaki <kuromi.1031blc@gmail.com>
2025-11-15 21:37:22 -0800

    new feature added

--- frontend/src/lib/highlight.util.ts
+++ frontend/src/lib/highlight.util.ts
@@ -1,9 +1,22 @@
 import type { Transaction } from "./util"
 
+export const renderHighlight= async()=>{
+    const data = await getRecentTransaction();
+    const count = numRecentTransaction(data);
+    const balance = recentBalance(data);
+    const highlightContainer =document.querySelector(".highlight-container")
+    if(!highlightContainer) return
+    highlightContainer.innerHTML=""
+
+    renderRecentTransaction(count)
+    renderRecentBalance(balance)
+}
+
   //get recent transaction
   export const getRecentTransaction = async()=>{
     const res = await fetch(`http://localhost:3500/transactions/recent`,{
-      method:"Get"
+      method:"Get",
+      credentials:"include"
     })
 
     if(!res.ok){
@@ -35,4 +48,25 @@ import type { Transaction } from "./util"
     const spending = recentSpending(data)
     const income = recentIncome(data)
     return income-spending
+  }
+
+  export const renderRecentTransaction =(count:number)=>{
+    const div = document.createElement('div')
+    div.className="highlight-border"
+
+    div.innerHTML=`
+    <div class="fw-bold">Recent Transaction</div>
+    <div class="counter-text">${count}</div>
+    `
+    document.querySelector(".highlight-container")?.append(div)
+  }
+
+  export const renderRecentBalance = (balance:number)=>{
+    const div = document.createElement('div')
+    div.className="highlight-border"
+    div.innerHTML =`
+    <div class="fw-bold">Recent Balance</div>
+    <div class="balance-text">$ ${balance.toFixed(2)}</div>
+    `
+    document.querySelector(".highlight-container")?.append(div)
   }
